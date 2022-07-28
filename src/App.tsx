26b6ab34e71418015930d1f3fed6dba021feeccf
Welisson Silveira <welissonmwse@gmail.com>
2022-07-28 10:43:27 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from 'styled-components'
-import { Home } from './pages/Home'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import { Cart } from './pages/Cart'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  )
+}
